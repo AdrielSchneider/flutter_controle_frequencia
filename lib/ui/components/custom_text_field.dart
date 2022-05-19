6eb8676edabcbd41ec2a2b00569b7f3d01b6eaf3
kevin120 <kevin.b.santos@gmail.com>
2022-05-18 22:18:36 -0300

    Adicionado tela de cadastro de professor, e a classe do mesmo.

--- lib/ui/components/custom_text_field.dart
+++ lib/ui/components/custom_text_field.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  CustomTextField({Key? key, required this.inputTitle, this.enabled = true})
+  CustomTextField({Key? key, required this.inputTitle, this.enabled = true, this.controller })
       : super(key: key);
 
   String inputTitle;
   bool enabled;
+  TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: TextFormField(
+        controller: controller,
         decoration: InputDecoration(
           labelText: inputTitle,
           enabled: enabled,
